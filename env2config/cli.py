d53d03c4f7cce689991442949236038aa0c7c55a
Daniel Collins <daniel.collins@viasat.com>
2015-07-08 16:04:09 -0700

    reorganize, packages are hard

--- env2config/cli.py
+++ env2config/cli.py
@@ -17,23 +17,21 @@ if sys.argv[1] not in ['build', 'inject']:
 
 if sys.argv[1] == 'build' and len(sys.argv) == 4:
     process, command, service_name, version = sys.argv
-    from env2config import build
+    from main import build
     build(service_name, version, './default_configs')
 
 if sys.argv[1] == 'build' and len(sys.argv) == 5:
     process, command, service_name, version, default_config_folder = sys.argv
-    from env2config import build
+    from main import build
     build(service_name, version, default_config_folder)
 
 
 if sys.argv[1] == 'inject' and len(sys.argv) == 2:
     process, command, default_config_folder = sys.argv
-    from env2config import inject
+    from main import inject
     inject('./default_configs')
 
 if sys.argv[1] == 'inject' and len(sys.argv) == 3:
     process, command, default_config_folder = sys.argv
-    from env2config import inject
+    from main import inject
     inject(default_config_folder)
-
-
